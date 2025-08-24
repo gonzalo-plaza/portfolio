3613c28a47e4b93b994e5edc4e6c534640b36e78
gonzalo-plaza <plazaruedag@gmail.com>
2025-08-24 13:03:27 +0200

    Main Page: Add MainSection and WorkExperienceSectionView

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
-import homeStyles from "@/app/styles/home.module.scss";
+import MainSectionView from "@/components/main-section/mainSectionView";
+import WorkExperienceSectionView from "@/components/work-experience-section/WorkExperienceSectionView";
 
 export default function Home() {
-  console.log(homeStyles);
   return (
     <main>
-      <section className={`container`}>
-        <div>
-          <h1>
-            👋 Hey, soy{" "}
-            <span className={`${homeStyles.homeSection__mainTitleHighlight}`}>
-              Gonzalo Plaza Rueda
-            </span>
-          </h1>
-          <p>
-            Sofware Engineer con +3 años de experiencia en Málaga, España. He
-            desarrollado distintos proyectos de frontend de diversos tipos,
-            dónde los más destacables han sido implementados en Ecommerce
-          </p>
-        </div>
-      </section>
+      <MainSectionView />
+      <WorkExperienceSectionView />
     </main>
   );
 }
