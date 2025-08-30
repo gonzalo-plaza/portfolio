2c138cd876bec7503f6fc792451e9d44c3277eb7
gonzalo-plaza <plazaruedag@gmail.com>
2025-08-30 14:07:14 +0200

    WorkExperieceDateTime: Creating the component

---
+++ src/components/work-experience-section/_components/WorkExperienceDateTime.tsx
@@ -0,0 +1,48 @@
+import { WorkExperience } from "@/models/workExperience/workExperience";
+import { getDifferenceTimeString } from "@/utils/dateUtils/dateUtils";
+
+import styles from "@/styles/components/work-experience-section/_components/work-experience-date-time.module.scss";
+
+interface WorkExperienceDateTimeProps {
+  startTime: WorkExperience["startTime"];
+  endTime: WorkExperience["endTime"];
+}
+
+const WorkExperienceDateTime = ({
+  startTime,
+  endTime,
+}: WorkExperienceDateTimeProps) => {
+  const [startTimeMonth, startTimeYear] = [
+    startTime.getMonth() + 1,
+    startTime.getFullYear(),
+  ];
+
+  const [endTimeMonth, endTimeYear] = [
+    endTime ? endTime?.getMonth() + 1 : null,
+    endTime?.getFullYear(),
+  ];
+
+  const startTimeString = `${startTimeMonth}/${startTimeYear}`;
+  const endTimeString = endTime ? `${endTimeMonth}/${endTimeYear}` : null;
+
+  let experienceTime = () => {
+    const endTimeToCheck = endTime ?? new Date();
+    return getDifferenceTimeString(endTimeToCheck, startTime, {
+      hideDays: true,
+    });
+  };
+  return (
+    <p>
+      <time dateTime={startTimeString}>{startTimeString}</time>
+      {" - "}
+      {endTimeString ? (
+        <time dateTime={endTimeString}>{endTimeString}</time>
+      ) : (
+        <span className={styles.workExperienceDateTimeBadge}>Actualmente</span>
+      )}
+      {" | "} <span>{experienceTime()}</span>
+    </p>
+  );
+};
+
+export default WorkExperienceDateTime;
