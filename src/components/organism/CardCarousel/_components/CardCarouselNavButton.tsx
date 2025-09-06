64484c56ddcb825d40e74150ad8f5122b45639a6
gonzalo-plaza <plazaruedag@gmail.com>
2025-09-06 13:06:22 +0200

    CardCarouselNavButton: Creating the component

---
+++ src/components/organism/CardCarousel/_components/CardCarouselNavButton.tsx
@@ -0,0 +1,35 @@
+import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
+
+import styles from "@/styles/components/organism/card-carousel/_components/card-carousel-nav-button.module.scss";
+import clsx from "clsx";
+import React, { forwardRef } from "react";
+
+interface CardCarouselNavButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  position: "right" | "left";
+  showArrow: boolean;
+}
+
+const CardCarouselNavButton = forwardRef<
+  HTMLButtonElement,
+  CardCarouselNavButtonProps
+>(({ position, className, showArrow = false, ...nativeProps }, ref) => {
+  const isRight = position === "right";
+  const isLeft = position === "left";
+  return (
+    <button
+      {...nativeProps}
+      ref={ref}
+      className={clsx(className, styles.cardCarouselNavButton, {
+        [styles.isRight]: isRight,
+        [styles.isLeft]: isLeft,
+        [styles.showArrow]: showArrow,
+      })}
+    >
+      {isLeft && <ArrowBigLeft />}
+      {isRight && <ArrowBigRight />}
+    </button>
+  );
+});
+
+export default CardCarouselNavButton;
