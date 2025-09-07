5e10a8f9f10df8e951e6adcf04bd0139479d4bb6
gonzalo-plaza <plazaruedag@gmail.com>
2025-09-07 17:10:46 +0200

    Project Model: Set optional fields

--- src/models/project/project.tsx
+++ src/models/project/project.tsx
@@ -5,12 +5,22 @@ export interface Project {
   title: string;
   description: string;
   projectTechnologyBadgeList: Array<ProjectTechonology>;
+  previewLink?: {
+    disabled?: boolean;
+    url: string;
+    title: string;
+  };
+  gitHubLink?: {
+    disabled?: boolean;
+    url: string;
+    title: string;
+  };
 }
 
 export interface ProjectTechonology {
   text: string;
-  icon: {
-    Component: (iconProps: React.SVGProps<SVGSVGElement>) => React.JSX.Element;
+  icon?: {
+    Component?: (iconProps: React.SVGProps<SVGSVGElement>) => React.JSX.Element;
     width?: number;
     height?: number;
   };
