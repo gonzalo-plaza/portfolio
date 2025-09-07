851d62cc94a21418554f18906c973cdce6df4c14
gonzalo-plaza <plazaruedag@gmail.com>
2025-09-07 17:08:43 +0200

    Button: Add styles

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,17 +2,34 @@ import clsx from "clsx";
 import React from "react";
 import { AsChildProps, Slot } from "../utils/Slot";
 
+import styles from "@/styles/components/atoms/button.module.scss";
+
 type ButtonProps = AsChildProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>
 > & {
   style?: React.CSSProperties;
   className?: string;
+  disabled?: boolean;
 };
 
-const Button = ({ asChild, className, ...nativeProps }: ButtonProps) => {
+const Button = ({
+  asChild,
+  className,
+  disabled,
+  ...nativeProps
+}: ButtonProps) => {
   const Comp = asChild ? Slot : "button";
 
-  return <Comp className={clsx(className)} {...nativeProps} />;
+  return (
+    <Comp
+      className={clsx(
+        styles.button,
+        { [styles.isDisabled]: disabled },
+        className
+      )}
+      {...nativeProps}
+    />
+  );
 };
 
 export default Button;
