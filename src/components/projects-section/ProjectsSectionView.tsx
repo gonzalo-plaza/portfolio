a37d4b6f6f87fd07e1ad2d6e644afe420069d7c5
gonzalo-plaza <plazaruedag@gmail.com>
2025-09-07 10:23:43 +0200

    ProjectsSectionView: Use projectList constant

--- src/components/projects-section/ProjectsSectionView.tsx
+++ src/components/projects-section/ProjectsSectionView.tsx
@@ -1,49 +1,7 @@
 import styles from "@/styles/components/projects-section/projects-section-view.module.scss";
 import CardCarousel from "../organism/CardCarousel/CardCarousel";
 import ProjectCard from "../organism/ProjectCard";
-
-const projects = [
-  {
-    id: 1,
-    imageUrl: "/images/garaje-plus-preview.jpg",
-    imageAlt: "GarajePlus",
-    title: "GarajePlus",
-    description:
-      "GarajePlus es un proyecto orientado a facilitar el registro de mantenimiento de vehículos. El objetivo es poder tener registrado todo el historial de mantenimientos, reparaciones y revisiones de cualquier vehiculo.",
-  },
-  {
-    id: 2,
-    imageUrl: "/images/garaje-plus-preview.jpg",
-    imageAlt: "GarajePlus",
-    title: "GarajePlus",
-    description:
-      "GarajePlus es un proyecto orientado a facilitar el registro de mantenimiento de vehículos. El objetivo es poder tener registrado todo el historial de mantenimientos, reparaciones y revisiones de cualquier vehiculo.",
-  },
-  {
-    id: 3,
-    imageUrl: "/images/garaje-plus-preview.jpg",
-    imageAlt: "GarajePlus",
-    title: "GarajePlus",
-    description:
-      "GarajePlus es un proyecto orientado a facilitar el registro de mantenimiento de vehículos. El objetivo es poder tener registrado todo el historial de mantenimientos, reparaciones y revisiones de cualquier vehiculo.",
-  },
-  {
-    id: 4,
-    imageUrl: "/images/garaje-plus-preview.jpg",
-    imageAlt: "GarajePlus",
-    title: "GarajePlus",
-    description:
-      "GarajePlus es un proyecto orientado a facilitar el registro de mantenimiento de vehículos. El objetivo es poder tener registrado todo el historial de mantenimientos, reparaciones y revisiones de cualquier vehiculo.",
-  },
-  {
-    id: 5,
-    imageUrl: "/images/garaje-plus-preview.jpg",
-    imageAlt: "GarajePlus",
-    title: "GarajePlus",
-    description:
-      "GarajePlus es un proyecto orientado a facilitar el registro de mantenimiento de vehículos. El objetivo es poder tener registrado todo el historial de mantenimientos, reparaciones y revisiones de cualquier vehiculo.",
-  },
-];
+import { projectList } from "@/constants/project/project";
 
 const ProjectsSectionView = () => {
   return (
@@ -59,19 +17,11 @@ const ProjectsSectionView = () => {
           💻 Proyectos
         </h2>
       </header>
-      {projects && (
+      {projectList && (
         <CardCarousel>
           <>
-            {projects.map((project) => {
-              return (
-                <ProjectCard
-                  key={project.id}
-                  imageUrl={project.imageUrl}
-                  imageAlt={project.imageAlt}
-                  title={project.title}
-                  description={project.description}
-                />
-              );
+            {projectList.map((project) => {
+              return <ProjectCard key={project.id} project={project} />;
             })}
           </>
         </CardCarousel>
